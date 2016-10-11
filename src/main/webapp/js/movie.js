1eda6878a2d29293b0bba6c0469a5ac22dd01a0b
Jon Pack <jon.pack@me.com>
2016-10-11 14:08:42 -0400

    Preliminary results display
    Updated code for displaying results on main HTML page.

--- src/main/webapp/js/movie.js
+++ src/main/webapp/js/movie.js
@@ -4,36 +4,33 @@ $(function() {
 					function() {
 						$.get('https://api.themoviedb.org/3/discover/movie?api_key=c9bd9d09ec5086253a01a6d67f5a1a75&language=en-US&primary_release_year=2016&with_genres='
 										+ $("#genre").val() ,
-		function(data) {
-			for (var i = 0; i < data.results.length; i++) {
-				data[i];
+								function(data) {
+									for (var i = 0; i < data.results.length; i++) {
+										data[i];
 
-				var id = data.results[i].id;
-				console.log('id : ', id);
+										var id = data.results[i].id;
+										console.log('id : ', id);
 
-				var title = data.results[i].title;
-				console.log('title : ', title);
+										var title = data.results[i].title;
+										console.log('title : ', title);
 
-				var vote_average = data.results[i].vote_average;
-				console.log('vote_averagey : ', vote_average);
+										var vote_average = data.results[i].vote_average;
+										console.log('vote_averagey : ', vote_average);
 
-				var backdrop_path = data.results[i].backdrop_path;
-				console.log('backdrop_path : ', backdrop_path);
+										var backdrop_path = data.results[i].backdrop_path;
+										console.log('backdrop_path : ', backdrop_path);
 
-				var overview = data.results[i].overview;
-				console.log('overview : ', overview);
+										var overview = data.results[i].overview;
+										console.log('overview : ', overview);
 
 
-				$('body').append(
-						"Name:   " + id + '<hr>' + "Title:   "
-								+ title + '<hr>' + "Vote Average:   "
-								+ vote_average + '<hr>'
-								+ "Path:    " + backdrop_path + '<hr>' + "Overview   " + overview + '<hr>'
-								+ '<hr><br><br>');
-
-			}
-
+										// $('body').append(
+										// 		"Name:   " + id + '<hr>' + "Title:   "
+										// 				+ title + '<hr>' + "Vote Average:   "
+										// 				+ vote_average + '<hr>'
+										// 				+ "Path:    " + backdrop_path + '<hr>' + "Overview   " + overview + '<hr>'
+										// 				+ '<hr><br><br>');
+									}
 								});
 					})
-
-		})
+})
