00c12212abd734f48344731c864da62d6ee99b10
Jon Pack <jon.pack@me.com>
2016-10-17 16:03:25 -0400

    Testing Commit.

--- src/main/webapp/js/zipcode.js
+++ src/main/webapp/js/zipcode.js
@@ -29,37 +29,69 @@ $(function(counter) {
 										
 										var promise = $.get("https://api.themoviedb.org/3/discover/movie?api_key=c9bd9d09ec5086253a01a6d67f5a1a75&language=en-US&primary_release_year=2016&with_genres="+ genre + ".json",
 												function(data) {
-									var htmlElements = "";
-									
-									// Populate the movie display table with movie suggestions.
-									for (var i = 0; i < data.results.length; i++) {
-										
+													var htmlElements = "";
+													
+													// Populate the movie display table with movie suggestions.
+													for (var i = 0; i < data.results.length; i++) {
+														
 
-										data[i];
+														data[i];
 
-										var id = data.results[i].id;
+														var id = data.results[i].id;
 
-										var title = data.results[i].title;
+														var title = data.results[i].title;
 
-										var vote_average = data.results[i].vote_average;
+														var vote_average = data.results[i].vote_average;
 
-										var backdrop_path = data.results[i].backdrop_path;
+														var poster_path = data.results[i].poster_path;
 
-										var overview = data.results[i].overview;
+														var overview = data.results[i].overview;
 
-								        var txt = "";
-								        txt += '<table><tr><td><img src="https://image.tmdb.org/t/p/w92' + backdrop_path + '"></img></td><td><a href="https://api.themoviedb.org/3/movie/' + id + '?api_key=c9bd9d09ec5086253a01a6d67f5a1a75" target="_blank">' + title + '</a></td><td>' + vote_average + '</td><td>' + overview + '</td></tr>';
-								        htmlElements += '<div id="div1">' + txt + '</div>';
-								    }
-								    var div1 = document.getElementById("div1");
-								    div1.innerHTML = htmlElements;
-									})
-									
-								});
+												        var txt = "";
+												        txt += '<table><tr><td><img src="https://image.tmdb.org/t/p/w92' + poster_path + '"></img></td><td><a href="#movieDetail"><button type="button" id="movieDetailButton">' + title + '</button></a></td><td>' + vote_average + '</td><td>' + overview + '</td></tr>';
+												        htmlElements += '<div id="div1">' + txt + '</div>';
+
+
+												    }
+												var div1 = document.getElementById("div1");
+												div1.innerHTML = htmlElements;
+												})
+									});
 
 					
 					});			
 			});
+$(function(movieDetailButton){
+															$("#movieDetailButton").click(
+																	function() {
+												                        var id = data.results[i].id;
+																		console.log(id),
+																		$.get("https://api.themoviedb.org/3/movie/" + id + "?api_key=c9bd9d09ec5086253a01a6d67f5a1a75",
+																			console.log(id),
+																			function(data) {
+
+
+
+																				
+																				var poster_path = data.results.poster_path;
+																				$("#poster_path").html(poster_path)	
+
+																				var title = data.results.title;
+																				$("#title").html(title);
+
+																				var tagline = data.results.tagline;
+																				$("#tagline").html(tagline);
+																				
+																				var overview = data.results[i].overview;
+																				$("#overview").html(overview)
+																				
+																				var imdb_id = "http://www.imdb.com/title/" + data.results.imdb_id;
+																				$("#imdb_id").html(imdb_id)
+																				
+																			})
+															})
+														})
 });
+
 
 
